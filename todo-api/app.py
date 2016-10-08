94d408707ba479f27bfe63944ea69ef016c7046e
AtulAneja <AtulAneja@Atuls-MBP.lfytina2.metronetinc.com>
2016-10-08 19:08:16 -0400

    Update on api
    Requires authentication to access data

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,8 +1,9 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
-
+auth = HTTPBasicAuth()
 tasks = [
     {
         'id': 1,
@@ -18,13 +19,24 @@ tasks = [
     }
 ]
 
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+    return jsonify({'tasks': tasks})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
