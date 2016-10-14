2f712d0a376084eca93e7d9da419e6fcb702308c
Tarang Khanna <khanna17@purdue.edu>
2016-10-14 14:14:06 -0400

    future prediction

--- MachineLearning/predictOnFutureValues.py
+++ MachineLearning/predictOnFutureValues.py
@@ -30,7 +30,6 @@ from sklearn.ensemble import RandomForestClassifier
 # import accelerate
 
 def PredictML(stocksDf, useSVM):
-
 	forecast_out = int(math.ceil(0.01*len(stocksDf))) # train 1% into future
 	print(forecast_out)
 
@@ -44,15 +43,19 @@ def PredictML(stocksDf, useSVM):
 	print stocksDf
 
 	stocksDf.to_csv('training.txt', sep='\t', encoding='utf-8')
-
+	
 	X = np.array(stocksDf.drop(['future'],1))
-	print(X.shape)
 	X = preprocessing.scale(X)
+	predict_index = len(X)-2
+	predict_value = X[predict_index-20:]
+	X = X[:predict_index-2]
+	print(X.shape)
+	
 	# X_lately = X[-forecast_out:]
 	# X = X[:-forecast_out:]
 
 	y = np.array(stocksDf['future']) # y is the 1% forcast 
-
+	y = y[:predict_index-2] # to keep consistent
 	print(X)
 	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2) # 20% training data, 80% testing 
 
@@ -63,17 +66,18 @@ def PredictML(stocksDf, useSVM):
 
 	print("Crunching...")
 
-	clf.fit(X,y)
-
-	accuracy = clf.score(X_test,y_test) # test on data not used for training
-	print(accuracy)
-
+	# clf.fit(X_train,y_train)
+	clf.fit(X,y) # all data till now
+	# accuracy = clf.score(X_test,y_test) # test on data not used for training, is around 95%
+	# print(accuracy)
+	print clf.predict(predict_value) # give array of last 10 days to get 1% into each values future
 	# print clf.predict() # predict into 1% future given todays ['Adj. Open','Adj. Close','S&P Open', 'Adj. Volume','Adj. High', 'Adj. Low']
+	# 2016-09-20 is Nan for future, use X values after that for prediction for current prediction
 
 if __name__ == "__main__":
-	df = quandl.get('Wiki/AAPL', authtoken="zzYfW2Zd_3J3Gt2o3Nz6", start_date="2010-12-12", end_date="2016-8-30")
+	df = quandl.get('Wiki/AAPL', authtoken="zzYfW2Zd_3J3Gt2o3Nz6", start_date="2010-12-12", end_date="2016-10-14")
 
-	sp500_df = quandl.get("YAHOO/INDEX_GSPC", authtoken="zzYfW2Zd_3J3Gt2o3Nz6", start_date="2010-12-12", end_date="2016-8-30")
+	sp500_df = quandl.get("YAHOO/INDEX_GSPC", authtoken="zzYfW2Zd_3J3Gt2o3Nz6", start_date="2010-12-12", end_date="2016-10-14")
 	df = df[['Adj. Open','Adj. Close', 'Adj. Volume','Adj. High', 'Adj. Low']]
 	sp500_df = sp500_df['Open']
 
