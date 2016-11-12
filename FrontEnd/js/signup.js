3957dcd542277432af3f3c5b48702a044b6e792f
posiden104 <posiden104@gmail.com>
2016-11-12 14:55:23 -0500

    glue signup not working

--- FrontEnd/js/signup.js
+++ FrontEnd/js/signup.js
@@ -1,4 +1,4 @@
-$signupForm;
+var $signupForm;
 
 $(document).ready(function() {
 
@@ -7,13 +7,45 @@ $(document).ready(function() {
 });
 
 function signup() {
-    var uri = "http://127.0.0.1:5000/ss/v1.0/login";
+    //  alert("here");
+    var uri = "http://127.0.0.1:5000/todo/api/v1.0/tasks";
     var info = $('form').serializeArray();
+    var email = ($("#email")[0].value);
+    var pwd = ($("#pwd")[0].value);
+    var cpwd = ($("#cpwd")[0].value);
+    var phone = ($("#phone")[0].value);
+    var fname = ($("#fname")[0].value);
+    var lname = ($("#lname")[0].value);
 
-    var dataToSend = {
-        "uname": uname,
-        "pswd": pswd
+    if (pwd != cpwd) {
+        alert("Passwords do not match.");
     }
+    var dataToSend = {
+        "firstName": fname,
+        "lastName": lname,
+        "age": "0",
+        "phoneNumber": phone,
+        "password": pwd,
+        "email": email
+    }
+    $.ajax({
+        url: uri,
+        method: "ADDUSER",
+        contentType: "application/json",
+        dataType: "json",
+        headers: {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "ADDUSER"
+        },
+        data: JSON.stringify(dataToSend),
+        success: function(data) {
+            dispName = data.response[0].dispName;
+            window.location.assign("account.html");
+        },
+        error: function(data) {
+            alert(data.responseJSON.response);
+        }
+    });
 
-    var data = post(uri, dataToSend);
 }
