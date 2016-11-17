826b2276b16b9e6202bcb1835fa5ca428644e52f
Wyatt Larkey <wlarkey@purdue.edu>
2016-11-17 00:09:44 -0500

    added dual colored lines

--- FrontEnd/lgraph.js
+++ FrontEnd/lgraph.js
@@ -1,57 +1,85 @@
 
-// CHART BY MIKE BOSTOCK //
-// I'll use this to test data and learn about d3 //
-// eventually I will replace with my own implementation //
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 50, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("#chart1 > svg"),
-    margin = {top: 20, right: 50, bottom: 30, left: 50},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+// parse the date / time
 var parseTime = d3.timeParse("%d-%b-%y");
 
-var x = d3.scaleTime()
-    .rangeRound([0, width]);
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
-
-var line = d3.line()
+// define the line
+var valueline = d3.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.close); });
 
-d3.tsv("GOOGL.txt", function(d) {
-  d.date = parseTime(d.date);
-  d.close = +d.close;
-  return d;
-}, function(error, data) {
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("#chart1 > svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.tsv("GOOGL.txt", function(error, data) {
   if (error) throw error;
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.close; }));
+  // format the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+  });
 
-  g.append("g")
-      .attr("class", "axis axis--x")
+  // Scale the range of the data
+  x.domain(
+      d3.extent(data, function(d) { return d.date; })
+    );
+  y.domain([
+      d3.min(data, function(d) { return d.close; }) - 10, 
+      d3.max(data, function(d) { return d.close; })
+  ]);
+
+  // set the gradient
+  svg.append("linearGradient")				
+    .attr("id", "line-gradient")			
+    .attr("gradientUnits", "userSpaceOnUse")	
+    .attr("x1", 0).attr("y1", y(0))			
+    .attr("x2", 900).attr("y2", y(0))		
+  .selectAll("stop")						
+    .data([									
+      {offset: "0%", color: "black"},		
+      {offset: "75%", color: "black"},		
+      {offset: "75%", color: "lawngreen"},	
+      {offset: "100%", color: "lawngreen"}	
+    ])					
+  .enter().append("stop")			
+    .attr("offset", function(d) { return d.offset; })	
+    .attr("stop-color", function(d) { return d.color; });
+
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // Add the X Axis
+  svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y))
-    .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .style("text-anchor", "end")
-      .text("Price ($)");
+  // Add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-  g.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
 });
+
+
 //////////////////////////////////////////////////////////////
 
 // RESIZE CHART
