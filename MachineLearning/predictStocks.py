716facf7f786903ff38d64fb5884bdb4ae4a0f04
Tarang Khanna <tarangalbert@gmail.com>
2016-12-03 14:35:08 -0500

    Removed prints

--- MachineLearning/predictStocks.py
+++ MachineLearning/predictStocks.py
@@ -40,7 +40,7 @@ class predictStocks:
 
 	def predictML(self,stocksDf, useRegression, symbol):
 		stocksDf = stocksDf.dropna(how='any')
-		print stocksDf
+		# print stocksDf
 		# X = np.array()
 		if useRegression:
 			X = np.array(stocksDf.drop(['Future'],1))
@@ -51,7 +51,7 @@ class predictStocks:
 		predict_index = len(X)-2
 		predict_value = X[predict_index-20:]
 		# X = X[:predict_index-2]
-		print(X.shape)
+		# print(X.shape)
 
 		# X_lately = X[-forecast_out:]
 		# X = X[:-forecast_out:]
@@ -79,7 +79,7 @@ class predictStocks:
 		# y = y[:predict_index-2] # to keep consistent
 		X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.5) # 50% training data, 50% testing 
 		
-		print y
+		# print y
 
 		best_clf = RandomForestClassifier(min_samples_leaf=2, n_estimators=100)
 		# best_clf = KNeighborsClassifier(n_neighbors=10, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=-1)
@@ -158,8 +158,8 @@ class predictStocks:
 					best_algo = 'XG'
 		file_name = 'XGClf_%s.pkl' %symbol
 		joblib.dump(best_clf, file_name) # save the classifier to file
-		print 'best accuracy:'
-		print best_accuracy
+		# print 'best accuracy:'
+		# print best_accuracy
 
 	def modelOptimization(self):	
 		# param_test1 = {
@@ -202,16 +202,16 @@ class predictStocks:
 		# use same preprocessing scale used while training
 		# predicted_df = pd.DataFrame(clf.predict(to_predict), columns=['Predicted_Winner'])
 		# predicted_df.to_csv('predictions.csv', encoding='utf-8')
-		print len(stocksDf['Adj. Close'].tail(predict_index))
+		# print len(stocksDf['Adj. Close'].tail(predict_index))
 		predicted_df = pd.DataFrame()
 		predicted_df['to_predict'] = stocksDf['Adj. Close'].tail(predict_index)
 		predicted_df = predicted_df.reset_index(drop=True)
-		print stocksDf['Adj. Close'].tail(predict_index)
+		# print stocksDf['Adj. Close'].tail(predict_index)
 		X = preprocessing.scale(X)
 		
 		predict_values = X[len(X)-predict_index:] # future for last set dates
 
-		print("Loading Classifier...")
+		# print("Loading Classifier...")
 
 		file_name = 'XGClf_%s.pkl' %symbol
 		clf = joblib.load(file_name)
@@ -226,23 +226,23 @@ class predictStocks:
 		le = preprocessing.LabelEncoder()
 		# le.classes_ = np.load('Label_Encoder.npy')
 
-		print result
+		# print result
 
 	# returns file name of the csv with predicted values
 	def regressionSaved(self,stocksDf, symbol, predict_index):
 		# predict_index = 14
 		stocksDf = stocksDf.dropna(how='any')
 		X = np.array(stocksDf)
-		print len(stocksDf['Adj. Close'].tail(predict_index))
+		# print len(stocksDf['Adj. Close'].tail(predict_index))
 		predicted_df = pd.DataFrame()
 		predicted_df['to_predict'] = stocksDf['Adj. Close'].tail(predict_index)
 		predicted_df = predicted_df.reset_index(drop=True)
-		print stocksDf['Adj. Close'].tail(predict_index)
+		# print stocksDf['Adj. Close'].tail(predict_index)
 		X = preprocessing.scale(X)
 		
 		predict_values = X[len(X)-predict_index:] # future for last predict_index dates
 
-		print("Loading Classifier...")
+		# print("Loading Classifier...")
 
 		file_name = 'LinearRegressionClf_%s.pkl' %symbol
 		clf = joblib.load(file_name)
@@ -254,7 +254,7 @@ class predictStocks:
 		predicted_df.to_csv(abs_path, encoding='utf-8')
 		result = pd.concat(frames, axis=1)
 
-		print result
+		# print result
 		return abs_path
 
 	def dailyReturn(self,data):
@@ -277,15 +277,15 @@ class predictStocks:
 	def download_data(self,symbol):
 		# use current date
 		till_date = time.strftime("%Y-%m-%d")
-		print 'Till Date:'
-		print till_date
+		# print 'Till Date:'
+		# print till_date
 
 		to_download = 'Wiki/%s' %symbol
 		df = quandl.get(to_download, authtoken="zzYfW2Zd_3J3Gt2o3Nz6", start_date="2010-12-12", end_date=till_date)
 
 		sp500_df_all = quandl.get("YAHOO/INDEX_GSPC", authtoken="zzYfW2Zd_3J3Gt2o3Nz6", start_date="2010-12-12", end_date=till_date)
 		df = df[['Adj. Open','Adj. Close', 'Adj. Volume','Adj. High', 'Adj. Low']]
-		print sp500_df_all
+		# print sp500_df_all
 		sp500_df = sp500_df_all[['Open', 'Adjusted Close']]
 
 		frames = [df, sp500_df]
@@ -293,7 +293,7 @@ class predictStocks:
 		df.columns = ['Adj. Open','Adj. Close','S&P Open','Adj. High', 'Adj. Low', 'Adj. Volume',  'S&P Adj. Close']
 		# add moving average
 		df = df.dropna(how='any')
-		print df
+		# print df
 		file_name = 'data/%s_training.csv' %symbol
 		df.to_csv(file_name, encoding='utf-8')
 
@@ -302,7 +302,7 @@ class predictStocks:
 		file_name = 'data/%s_training.csv' %stockName
 		read_df = pd.read_csv(file_name, index_col = "Date")
 		# forecast_out = 14
-		print 'predicting into: ' + str(forecast_out)
+		# print 'predicting into: ' + str(forecast_out)
 		read_df['Daily Returns'] = self.dailyReturn(read_df['Adj. Close'])
 		to_predict_df = read_df.copy(deep=True)
 		
@@ -313,10 +313,51 @@ class predictStocks:
 		self.predictML(read_df, True, stockName)
 		return self.regressionSaved(to_predict_df, stockName, forecast_out)
 
+	# def stocksClassify(self, stockName, forecast_out):
+	# 	self.download_data(stockName)
+	# 	file_name = 'data/%s_training.csv' %stockName
+	# 	read_df = pd.read_csv(file_name, index_col = "Date")
+	# 	# forecast_out = 14
+	# 	print 'predicting into: ' + str(forecast_out)
+	# 	read_df['Daily Returns'] = self.dailyReturn(read_df['Adj. Close'])
+	# 	to_predict_df = read_df.copy(deep=True)
+		
+	# 	read_df['Future'] = read_df['Adj. Close'].shift(-forecast_out)
+
+	# 	read_df = read_df.dropna(how='any')
+
+	# 	decisions = []
+	# 	pe_ratio = []
+	# 	for index, row in read_df.iterrows():
+	# 		# floating point comparison careful
+	# 		# if 3 % increase in two weeks, then classify as a buy
+	# 		# another method is to get historical buy-sell ratings
+	# 		if (round(row['Future'],3) > round((1.03*row['Adj. Close']),3)):
+	# 			decisions.append('Buy')
+	# 		elif (round(row['Future'],3) < ((1.03*row['Adj. Close']),3)):
+	# 			decisions.append('Sell')
+	# 		else:
+	# 			decisions.append('Hold')
+
+	# 	# read_df['P/E Ratio'] = pe_ratio	
+	# 	read_df_binary = read_df.copy(deep=True)
+	# 	read_df_binary['Decision'] = decisions
+	# 	print read_df_binary['Decision'].value_counts()
+
+	# 	read_df_regression = read_df.copy(deep=True)
+
+	# 	read_df_binary = read_df_binary.drop(['Future'], axis=1)
+
+
+	# 	read_df = read_df.dropna(how='any')
+
+	# 	self.predictML(read_df, True, stockName)
+	# 	return self.regressionSaved(to_predict_df, stockName, forecast_out)
+
 if __name__ == "__main__":
 	predict = predictStocks()
 	symbol = 'GOOGL'
-	print predict.stocksRegression(symbol, 40)
+	print predict.stocksRegression(symbol, 14)
 	# symbols = ['AAPL', 'GOOGL', 'GLD']
 	# symbol = 'GOOGL'
 	# download_data(symbol)
